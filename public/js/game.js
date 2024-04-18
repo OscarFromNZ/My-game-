1aaed9a64eacc1a3d5c4881d2ee91b9a286d1d73
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2024-04-18 22:10:57 +1200

    did some awesome stuff

--- public/js/game.js
+++ public/js/game.js
@@ -3,15 +3,12 @@ class Game {
         console.log("Hi there from Game!");
         this.socket = io();
 
-        this.velocity = 2; // for testing
-        this.rotationSpeed = 0.02;
-        this.rotationAcceleration = 0.001;
-        this.maxRotationSpeed = 0.3;
-        
-        this.firstLoop = true; 
+        this.velocity = 5; // for testing
+
+        this.firstLoop = true;
         //this.angularVelocity = 0;
-        this.maxAngularVelocity = 0.1;
-        
+        this.maxAngularVelocity = 0.15;
+
         this.mouseX = undefined;
         this.mouseY = undefined;
     }
@@ -29,7 +26,7 @@ class Game {
         this.app.stage.hitArea = this.app.screen;
 
         this.app.stage.on('pointermove', this.onPointerMove);
-        this.app.stage.on('keydown', this.onKeyDown);
+        document.addEventListener('keydown', this.onKeyDown);
 
         this.app.ticker.add(this.gameLoop);
 
@@ -39,11 +36,13 @@ class Game {
     }
 
     async setupBunny() {
-        const texture = await PIXI.Assets.load('https://pixijs.com/assets/bunny.png');
+        const texture = await PIXI.Assets.load('https://i.imgur.com/3EqkexE.png');
 
         this.bunny = new PIXI.Sprite(texture);
 
         this.bunny.anchor.set(0.5);
+        this.bunny.scale.set(0.05);
+
         this.bunny.x = this.app.screen.width / 2;
         this.bunny.y = this.app.screen.height / 2;
 
@@ -56,7 +55,24 @@ class Game {
     }
 
     onKeyDown = (e) => {
-        console.log('valid function ig');
+        console.log('Key pressed:', e.key);
+        // Handle different keys with a switch statement or if conditions
+        switch (e.key) {
+            case 'w':
+                var change = 0.01;
+                if ((this.bunny.scale.x + change) < 1) {
+                    this.bunny.scale.set(this.bunny.scale.x + change);
+                    this.velocity -= change * 10;
+                }
+                break;
+            case 's':
+                var change = -0.01;
+                if ((this.bunny.scale.x + change) > 0) {
+                    this.bunny.scale.set(this.bunny.scale.x + change);
+                    this.velocity -= change * 10;
+                }
+                break;
+        }
     }
 
 
@@ -96,34 +112,36 @@ class Game {
         if (!this.bunny || !this.mouseX || !this.mouseY || !this.bunny.x || !this.bunny.y) {
             return console.error('Missing bunny or mouse coordinates.');
         }
-    
+
         // angle it wants to be at
         let angleToCursorRad = Math.atan2(this.mouseY - this.bunny.y, this.mouseX - this.bunny.x);
-    
+
+
         if (this.firstLoop) {
             console.log('bunny rotation setup');
-            this.bunny.rotation = angleToCursorRad + 1/2 * Math.PI;
+            this.bunny.rotation = angleToCursorRad;
             this.firstLoop = false;
             return;
         }
-    
+
         // shortest angle difference between em
-        let angleDifferenceRad = (angleToCursorRad - this.bunny.rotation) % (2 * Math.PI) + 1/2 * Math.PI;
+        //let angleDifferenceRad = ((angleToCursorRad - this.bunny.rotation) + Math.PI) % (2 * Math.PI) - Math.PI;
+
+        let diff = (angleToCursorRad - this.bunny.rotation + Math.PI) % (2 * Math.PI) - Math.PI;
+        let angleDifferenceRad = diff < -Math.PI ? diff + 2 * Math.PI : diff; // Normalize to -pi to +pi
+
+        let direction = Math.sign(angleDifferenceRad); // neg counter pos clockwise
 
         // because of fun velocity restrictions
-        let maxChangeInAngle = this.maxAngularVelocity * time.deltaTime; 
+        let maxChangeInAngle = this.maxAngularVelocity * time.deltaTime;
 
         // using the fun velocity restriction
-        let changeInAngle = angleDifferenceRad;
-        
-        if (Math.abs(angleDifferenceRad) > maxChangeInAngle) {
-            changeInAngle = Math.sign(angleDifferenceRad) * maxChangeInAngle;
-        }
-    
+        let changeInAngle = Math.min(Math.abs(angleDifferenceRad), maxChangeInAngle) * direction;
+
         this.bunny.rotation += changeInAngle;
-    
+
         const distance = this.velocity * time.deltaTime;
-        this.bunny.x += Math.cos(this.bunny.rotation - Math.PI / 2) * distance;
-        this.bunny.y += Math.sin(this.bunny.rotation - Math.PI / 2) * distance;
-    }    
+        this.bunny.x += Math.cos(this.bunny.rotation) * distance;
+        this.bunny.y += Math.sin(this.bunny.rotation) * distance;
+    }
 }
